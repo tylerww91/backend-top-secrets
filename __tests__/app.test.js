b6075d58d6296b09431f9c2e04b1a46b71ca9e75
Tyler Watson <tylerww91@gmail.com>
2022-11-16 10:23:51 -0800

    builds create a new user test and mockUser object

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,32 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const { request } = require('express');
+const request = require('supertest');
+const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+const mockUser = {
+  firstName: 'Test',
+  lastName: 'User',
+  email: 'example@test.com',
+  password: '123654',
+};
+
+describe('top-secret routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  it('create a new user', async () => {
+    const res = await request(app).post('/api/v1/users').send(mockUser);
+    const { firstName, lastName, email } = mockUser;
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      firstName,
+      lastName,
+      email,
+    });
   });
+
   afterAll(() => {
     pool.end();
   });
