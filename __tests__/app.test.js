85f48571e549dc34f44a56b7afe0dbecea091b66
Tyler Watson <tylerww91@gmail.com>
2022-11-16 15:44:40 -0800

    builds POST to /api/v1/users route that creates a new user

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+// const UserService = require('../lib/services/UserService');
 
 const mockUser = {
   firstName: 'Test',
@@ -9,6 +10,18 @@ const mockUser = {
   email: 'example@test.com',
   password: '123654',
 };
+
+/// for returns the current user route
+// const registerAndLogin = async (userProps = {}) => {
+//   const password = userProps.password ?? mockUser.password;
+
+//   const agent = request.agent(app);
+
+//   const user = await UserService.create({ ...mockUser, ...userProps});
+
+//   const { email } = user;
+//   await agent.post('/api/v1/users/sessions').send({ email, password })
+// }
 
 describe('top-secret routes', () => {
   beforeEach(() => {
