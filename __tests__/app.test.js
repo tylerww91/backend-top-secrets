f27d7576d295faf470c8bb491bf4d4f280aaa967
Tyler Watson <tylerww91@gmail.com>
2022-11-16 22:48:42 -0800

    builds authenticate middleware, and builds GET /me route that returns the current user in order to check sign out

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-// const UserService = require('../lib/services/UserService');
+const UserService = require('../lib/services/UserService');
 
 const mockUser = {
   firstName: 'Test',
@@ -11,17 +11,17 @@ const mockUser = {
   password: '123654',
 };
 
-// const registerAndLogin = async (userProps = {}) => {
-//   const password = userProps.password ?? mockUser.password;
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
 
-//   const agent = request.agent(app);
+  const agent = request.agent(app);
 
-//   const user = await UserService.create({ ...mockUser, ...userProps });
+  const user = await UserService.create({ ...mockUser, ...userProps });
 
-//   const { email } = user;
-//   await agent.post('/api/v1/users/sessions').send({ email, password });
-//   return [agent, user];
-// };
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  return [agent, user];
+};
 
 describe('top-secret routes', () => {
   beforeEach(() => {
@@ -60,16 +60,16 @@ describe('top-secret routes', () => {
     expect(resp.status).toBe(204);
   });
 
-  // it('returns the current user', async () => {
-  //   const [agent, user] = await registerAndLogin();
-  //   const me = await agent.get('/api/v1/users/me');
+  it('returns the current user', async () => {
+    const [agent, user] = await registerAndLogin();
+    const me = await agent.get('/api/v1/users/me');
 
-  //   expect(me.body).toEqual({
-  //     ...user,
-  //     exp: expect.any(Number),
-  //     iat: expect.any(Number),
-  //   });
-  // });
+    expect(me.body).toEqual({
+      ...user,
+      exp: expect.any(Number),
+      iat: expect.any(Number),
+    });
+  });
 
   afterAll(() => {
     pool.end();
