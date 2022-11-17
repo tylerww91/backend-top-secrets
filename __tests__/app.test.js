c52bd221b623e081972a34b69be4391c4663814b
Tyler Watson <tylerww91@gmail.com>
2022-11-16 17:49:28 -0800

    builds POST to /api/v1/users/sessions route to sign in an existing user

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -11,17 +11,17 @@ const mockUser = {
   password: '123654',
 };
 
-/// for returns the current user route
 // const registerAndLogin = async (userProps = {}) => {
 //   const password = userProps.password ?? mockUser.password;
 
 //   const agent = request.agent(app);
 
-//   const user = await UserService.create({ ...mockUser, ...userProps});
+//   const user = await UserService.create({ ...mockUser, ...userProps });
 
 //   const { email } = user;
-//   await agent.post('/api/v1/users/sessions').send({ email, password })
-// }
+//   await agent.post('/api/v1/users/sessions').send({ email, password });
+//   return [agent, user];
+// };
 
 describe('top-secret routes', () => {
   beforeEach(() => {
@@ -30,6 +30,7 @@ describe('top-secret routes', () => {
 
   it('create a new user', async () => {
     const res = await request(app).post('/api/v1/users').send(mockUser);
+    expect(res.status).toBe(200);
     const { firstName, lastName, email } = mockUser;
     expect(res.body).toEqual({
       id: expect.any(String),
@@ -38,6 +39,25 @@ describe('top-secret routes', () => {
       email,
     });
   });
+
+  it('POST /api/v1/sessions signs in an existing user', async () => {
+    await request(app).post('/api/v1/users').send(mockUser);
+    const res = await request(app)
+      .post('/api/v1/users/sessions')
+      .send({ email: 'example@test.com', password: '123654' });
+    expect(res.status).toEqual(200);
+  });
+
+  // it('returns the current user', async () => {
+  //   const [agent, user] = await registerAndLogin();
+  //   const me = await agent.get('/api/v1/users/me');
+
+  //   expect(me.body).toEqual({
+  //     ...user,
+  //     exp: expect.any(Number),
+  //     iat: expect.any(Number),
+  //   });
+  // });
 
   afterAll(() => {
     pool.end();
